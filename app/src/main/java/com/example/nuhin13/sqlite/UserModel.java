05672622f1f226e40b31ff9dff9a4c8c76c1b427
Nuhin13 <nuhin.startern@gmail.com>
2017-06-18 12:57:50 +0600

    initial commit

---
+++ app/src/main/java/com/example/nuhin13/sqlite/UserModel.java
@@ -0,0 +1,53 @@
+package com.example.nuhin13.sqlite;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable{
+
+    private String name;
+    private String hobby;
+    private String city;
+    private String address;
+    private int id;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
